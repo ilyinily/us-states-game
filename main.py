b874d7dee8205b05a2bd6474c9c5599f74deeadd
ilyinily <ilyinily@gmail.com>
2020-12-02 22:59:58 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from turtle import Turtle, Screen
+import pandas as pd
+
+# Initial setup
+screen = Screen()
+twix = Turtle()
+twix.hideturtle()
+twix.penup()
+screen.bgpic(picname="./blank_states_img.gif")
+
+# Getting data from the dataset
+dataset = pd.read_csv("./50_states.csv")
+
+# Formatting the state names to lowercase for convenience
+for i in range(len(dataset['state'])):
+    dataset['state'][i] = dataset['state'][i].lower()
+
+correct_answers = set()
+misses = set()
+
+game_continues = True
+while game_continues:
+    answer = screen.textinput(title="Guess the states", prompt="Enter the state:")
+    if answer.lower() in dataset['state'].values:
+        correct_answers.add(answer)
+        twix.setposition(x=int(dataset[dataset['state'] == answer]['x']), y=int(dataset[dataset['state'] == answer]['y']))
+        twix.write(arg=answer)
+    elif answer.lower() == "exit":
+        game_continues = False
+    else:
+        misses.add(answer.lower())
+    if len(correct_answers) == 50:
+        game_continues = False
+
+print(f"Game over.\nYou have successfully guessed {len(correct_answers)} states.")
+
+screen.exitonclick()
